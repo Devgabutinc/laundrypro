601aa9534d115a81943da2bd8c169304226652d4
LaundryPro Developer <devgabutinc@gmail.com>
2025-05-26 23:36:47 +0700

    Fix: Menambahkan Directory dan konstanta lain di modul shim Capacitor

--- src/utils/capacitor-shims/empty-module.js
+++ src/utils/capacitor-shims/empty-module.js
@@ -1,6 +1,25 @@
 // Empty module to replace Capacitor plugins in web builds
 // This file provides dummy implementations of Capacitor plugins for web builds
 
+// Directory enum yang dibutuhkan oleh Filesystem
+export const Directory = {
+  Documents: 'DOCUMENTS',
+  Data: 'DATA',
+  Cache: 'CACHE',
+  External: 'EXTERNAL',
+  ExternalStorage: 'EXTERNAL_STORAGE'
+};
+
+// FilesystemEncoding enum
+export const FilesystemEncoding = {
+  UTF8: 'utf8',
+  ASCII: 'ascii',
+  UTF16: 'utf16'
+};
+
+// FilesystemDirectory (deprecated) untuk backward compatibility
+export const FilesystemDirectory = Directory;
+
 // Default export
 export default {
   // Common methods
@@ -33,6 +52,11 @@ export default {
   // Filesystem
   readFile: async () => console.log('Capacitor plugin shim: Filesystem.readFile called'),
   writeFile: async () => console.log('Capacitor plugin shim: Filesystem.writeFile called'),
+  getUri: async () => ({ uri: '' }),
+  mkdir: async () => console.log('Capacitor plugin shim: Filesystem.mkdir called'),
+  rmdir: async () => console.log('Capacitor plugin shim: Filesystem.rmdir called'),
+  readdir: async () => ({ files: [] }),
+  stat: async () => ({ type: 'file', size: 0, ctime: 0, mtime: 0, uri: '' }),
   
   // Local Notifications
   schedule: async () => console.log('Capacitor plugin shim: LocalNotifications.schedule called'),
@@ -70,10 +94,13 @@ export const Capacitor = {
 export const PushNotifications = {
   register: async () => console.log('Capacitor plugin shim: PushNotifications.register called'),
   addListener: () => ({ remove: () => {} }),
+  removeAllListeners: () => {},
+  checkPermissions: async () => ({ receive: 'granted' }),
+  requestPermissions: async () => ({ receive: 'granted' }),
 };
 
 export const Toast = {
-  show: async () => console.log('Capacitor plugin shim: Toast.show called'),
+  show: async (options) => console.log('Capacitor plugin shim: Toast.show called', options),
 };
 
 export const Share = {
@@ -81,11 +108,23 @@ export const Share = {
 };
 
 export const Filesystem = {
-  readFile: async () => console.log('Capacitor plugin shim: Filesystem.readFile called'),
+  readFile: async () => ({ data: '' }),
   writeFile: async () => console.log('Capacitor plugin shim: Filesystem.writeFile called'),
+  getUri: async () => ({ uri: '' }),
+  mkdir: async () => console.log('Capacitor plugin shim: Filesystem.mkdir called'),
+  rmdir: async () => console.log('Capacitor plugin shim: Filesystem.rmdir called'),
+  readdir: async () => ({ files: [] }),
+  stat: async () => ({ type: 'file', size: 0, ctime: 0, mtime: 0, uri: '' }),
+  appendFile: async () => console.log('Capacitor plugin shim: Filesystem.appendFile called'),
+  deleteFile: async () => console.log('Capacitor plugin shim: Filesystem.deleteFile called'),
+  copy: async () => console.log('Capacitor plugin shim: Filesystem.copy called'),
+  downloadFile: async () => ({ path: '' }),
 };
 
 export const LocalNotifications = {
   schedule: async () => console.log('Capacitor plugin shim: LocalNotifications.schedule called'),
   addListener: () => ({ remove: () => {} }),
+  removeAllListeners: () => {},
+  checkPermissions: async () => ({ display: 'granted' }),
+  requestPermissions: async () => ({ display: 'granted' }),
 };
