f501b7ed8d9bf87aaa2475fa4d863c2e70fd77d2
LaundryPro Developer <devgabutinc@gmail.com>
2025-05-28 15:34:35 +0700

    Hapus folder android dan proguard-rules.pro untuk mengatasi masalah printing dengan angka random

--- android/app/src/main/assets/public/assets/index-Dm9A5lE5.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BcGZlol1.js","assets/index-Can26OEw.js","assets/index-MK1v0ErH.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-Can26OEw.js";const o=r("Share",{web:()=>t(()=>import("./web-BcGZlol1.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
